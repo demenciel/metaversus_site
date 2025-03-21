fd2f338e111f52bc8b14e456928765a8f5944578
Alex Couture <79432799+demenciel@users.noreply.github.com>
2025-03-21 13:58:47 -0400

    Fix: Optimize page title for SEO.

--- app/head.js
+++ app/head.js
@@ -1,9 +1,3 @@
-const Head = () => (
-  <>
-    <title>Metaversus</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/favicon.ico" />
-  </>
-);
-
-export default Head;
+```jsx
+<title>Metaversus: Explore the Virtual Reality World - Join Now</title>
+```
