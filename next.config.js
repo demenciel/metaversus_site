bcd4f9809a395acfc382297af76420638be22d28
Alex Couture <79432799+demenciel@users.noreply.github.com>
2025-03-21 12:24:28 -0400

    Fix: Add Schema.org structured data.

--- next.config.js
+++ next.config.js
@@ -6,3 +6,16 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+
+```javascript
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "url": "https://www.example.com",
+  "name": "Example Website",
+  "description": "This is an example website description."
+}
+</script>
+```
