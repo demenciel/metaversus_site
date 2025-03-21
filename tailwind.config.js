bb24433506832a10f9f69d1f4b18ac3eefb34201
Alex Couture <79432799+demenciel@users.noreply.github.com>
2025-03-21 13:59:30 -0400

    Fix: Add Open Graph meta tags.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,3 +5,9 @@
 <h1 class="text-primary-black">Main Topic - Website Name</h1>
 ```
 <link rel="canonical" href="">
+```html
+<meta property="og:title" content="Your Page Title" />
+<meta property="og:description" content="Your page description." />
+<meta property="og:image" content="URL_to_your_image.jpg" />
+<meta property="og:url" content="Your_page_URL" />
+```
