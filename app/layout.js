05e6dadaf08e4743775c96e4944343acff19d353
Alex Couture <79432799+demenciel@users.noreply.github.com>
2025-03-21 12:18:08 -0400

    Fix: Add canonical URL to prevent duplicate content.

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import '../styles/globals.css';
 const RootLayout = ({ children }) => (
   <html lang="en">
     <head>
+<link rel="canonical" href="{{website_url}}">
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
     </head>
