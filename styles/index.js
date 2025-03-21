0f1717a4622de6997d30d70f370570e6d5ad2441
Alex Couture <79432799+demenciel@users.noreply.github.com>
2025-03-21 12:24:38 -0400

    Fix: Add Schema.org structured data.

--- styles/index.js
+++ styles/index.js
@@ -21,3 +21,20 @@ const styles = {
 };
 
 export default styles;
+
+
+```jsx
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "name": "Your Website Name",
+  "url": "https://www.yourwebsite.com",
+  "potentialAction": {
+    "@type": "SearchAction",
+    "target": "https://www.yourwebsite.com/search?q={search_term_string}",
+    "query-input": "required name=search_term_string"
+  }
+}
+</script>
+```
