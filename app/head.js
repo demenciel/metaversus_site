b89436e719757d236afe76b903c578eb1e0a2e9b
Alex Couture <79432799+demenciel@users.noreply.github.com>
2025-03-21 13:58:51 -0400

    Fix: Add Open Graph meta tags.

--- app/head.js
+++ app/head.js
@@ -5,3 +5,9 @@
 <h1>Metaversus - Explore the Metaverse</h1>
 ```
 <link rel="canonical" href="">
+```jsx
+<meta property="og:title" content="Metaversus" />
+<meta property="og:description" content="Description of Metaversus" />
+<meta property="og:image" content="URL_to_image" />
+<meta property="og:url" content="URL_to_page" />
+```
