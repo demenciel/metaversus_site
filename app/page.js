9b44bac7720df4fb21beb173afdb147d4ee7f28d
Alex Couture <79432799+demenciel@users.noreply.github.com>
2025-03-21 13:59:05 -0400

    Fix: Optimize page title for SEO.

--- app/page.js
+++ app/page.js
@@ -1,29 +1,3 @@
-import { Footer, Navbar } from '../components';
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
-
-const Page = () => (
-  <div className='bg-primary-black overflow-hidden'>
-    <Navbar />
-    <Hero />
-    <div className='relative'>
-      <About />
-      <div className='gradient-03 z-0' />
-      <Explore />
-    </div>
-    <div className='relative'>
-      <GetStarted />
-      <div className='gradient-04 z-0' />
-      <WhatsNew />
-    </div>
-    <div className='relative'>
-      <World />
-      <div className='gradient-04 z-0' />
-      <Insights />
-      <div className='gradient-05 z-0' />
-    </div>
-    <Feedback />
-    <Footer />
-  </div>
-);
-
-export default Page;
+```jsx
+<title>Your Ultimate Guide to Exploring the World - Stay Informed</title>
+```
