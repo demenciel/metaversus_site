ac56136f35376ed325b8c7d466b42cf878bc785b
Alex Couture <79432799+demenciel@users.noreply.github.com>
2025-03-21 12:18:43 -0400

    Fix: Add Schema.org structured data.

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,3 +20,15 @@ module.exports = {
   },
   plugins: [],
 };
+
+
+```html
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "name": "Your Website Name",
+  "url": "https://www.yourwebsite.com"
+}
+</script>
+```
