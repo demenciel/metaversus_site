ad800e61d88715f9c07aa71891d37423b7daeaf4
Alex Couture <79432799+demenciel@users.noreply.github.com>
2025-03-21 13:59:09 -0400

    Fix: Add Twitter Card meta tags.

--- app/page.js
+++ app/page.js
@@ -1,44 +1,13 @@
-```jsx
-<meta name="description" content="Discover insights, explore new worlds, and get started with our innovative solutions. Join us on a journey of feedback and growth." />
+<p>```jsx
+<meta name="description" content="Discover insights, explore new worlds, and get started with our innovative solutions. Join us on a journey of feedback and growth.">
 ```
 ```jsx
-<h1 className='sr-only'>Main Topic - Website Name</h1>
+</p><h1 classname="sr-only">Main Topic - Website Name</h1><p>
 ```
 <link rel="canonical" href="">
 ```jsx
 import Head from 'next/head';
 
-const Page = () => (
-  <>
-    <Head>
-      <meta property="og:title" content="Your Page Title" />
-      <meta property="og:description" content="Your page description." />
-      <meta property="og:image" content="URL_to_your_image" />
-      <meta property="og:url" content="Your_page_URL" />
-      <meta property="og:type" content="website" />
-    </Head>
-    <div className='bg-primary-black overflow-hidden'>
-      <Navbar />
-      <Hero />
-      <div className='relative'>
-        <About />
-        <div className='gradient-03 z-0' />
-        <Explore />
-      </div>
-      <div className='relative'>
-        <GetStarted />
-        <div className='gradient-04 z-0' />
-        <WhatsNew />
-      </div>
-      <div className='relative'>
-        <World />
-        <div className='gradient-04 z-0' />
-        <Insights />
-        <div className='gradient-05 z-0' />
-      </div>
-      <Feedback />
-      <Footer />
-    </div>
-  </>
-);
-```
+const Page = () =&gt; (
+  &lt;&gt;
+    </p>
